db5d9e4d86b6055d50fcdb9414a9ab69959196a1
dappone <dappone2019@hotmail.com>
2020-07-28 15:20:23 +0800

    test

--- config/queue.php
+++ config/queue.php
@@ -59,7 +59,7 @@ return [
 
         'redis' => [
             'driver' => 'redis',
-            'connection' => env('QUEUE_REDIS_CONNECTION', 'redis'),
+            'connection' => env('QUEUE_REDIS_CONNECTION', 'default'),
             'queue' => 'default',
             'retry_after' => 90,
             'block_for' => null,
