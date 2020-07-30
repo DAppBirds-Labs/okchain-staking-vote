a3fd6b5036946d6a12a146c87a1656b1b948583f
dappone <dappone2019@hotmail.com>
2020-07-30 20:19:16 +0800

    td

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'validator_logo' => 'http://image.cd.mmzhuli.com/895444397c2e8aecb066dc7aced81223?hjw=520&hjh=520',
+    'validator_logo' => 'http://image.cd.mmzhuli.com/20c1800fdfa10500c3e49ae991217af5?hjw=80&hjh=80',
     'bond_denom' => 'tokt',
     'bond_denom_logo' => 'http://jdgjimage-1251031594.image.myqcloud.com/9cb726dd88f1430c99861cf5c09d2f1f?hjw=72&hjh=72',
 ];
