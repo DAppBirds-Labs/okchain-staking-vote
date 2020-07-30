8215b4b08d93d7c83e748599c83364dd84dc0114
dappone <dappone2019@hotmail.com>
2020-07-30 21:30:27 +0800

    td

--- config/app.php
+++ config/app.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'validator_logo' => 'http://image.cd.mmzhuli.com/20c1800fdfa10500c3e49ae991217af5?hjw=80&hjh=80',
+    'validator_logo' => 'http://image.cd.mmzhuli.com/8328b8cbe6f630757f177b6c6a7c82aa?hjw=200&hjh=200',
     'bond_denom' => 'tokt',
     'bond_denom_logo' => 'http://jdgjimage-1251031594.image.myqcloud.com/9cb726dd88f1430c99861cf5c09d2f1f?hjw=72&hjh=72',
 ];
